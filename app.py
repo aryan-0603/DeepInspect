8e50d524403da6b9f3a178157498f4dba86311ba
aryan-0603 <114023565+aryan-0603@users.noreply.github.com>
2025-03-07 23:02:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def dashboard():
     df = pd.read_csv('data/network_traffic_with_anomalies.csv')
-    traffic_data = df.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
+    traffic_data = df.to_dict(orient='records')  
     return render_template('index.html', traffic_data=traffic_data)
 
 if __name__ == '__main__':
